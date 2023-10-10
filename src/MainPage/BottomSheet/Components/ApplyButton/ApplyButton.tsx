70e5ac9f5bf9c2daa31613e86de1c241d11f7b60
hyorish03 <hyorish2001@gmail.com>
2023-10-10 14:19:06 +0900

    Feat: 매칭신청버튼 UI 개발

---
+++ src/MainPage/BottomSheet/Components/ApplyButton/ApplyButton.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+function ApplyButton() {
+  return (
+    <Wrapper>
+      <Button type="button">매칭 신청하기</Button>
+    </Wrapper>
+  );
+}
+const Wrapper = styled.div`
+  width: 100%;
+  height: 128px;
+  background-color: white;
+  bottom: 0;
+  display: flex;
+  position: fixed;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  width: 335px;
+  height: 48px;
+  flex-shrink: 0;
+  border-radius: 12px;
+  background: var(--Green-Button, #1edd81);
+  border: none;
+  font-size: 16px;
+  color: white;
+  color: #fff;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: 0.54px;
+`;
+export default ApplyButton;
