a49d0de2c740e6d572ef00305e32d30aae767b04
wldnjs7064 <wldnjs7064@naver.com>
2023-10-28 17:46:18 +0900

    feat: bottomSheet 완성

--- src/Pages/CreatePotPage/Components/Map/DestinationPage.tsx
+++ src/Pages/CreatePotPage/Components/Map/DestinationPage.tsx
@@ -4,6 +4,7 @@ import { Chevronleft } from "../../../../assets/svg";
 import { useRef } from "react";
 import { HEADER_HEIGHT } from "../../../../Constants/constant";
 import { useNavigate } from "react-router-dom";
+import BottomSheet from "./BottomSheet";
 
 function DestinationPage() {
   const navigate = useNavigate();
@@ -18,7 +19,9 @@ function DestinationPage() {
       </Header>
       <Body>
         <Kakaomap mapRef={mapRef} />
-        <Bottom></Bottom>
+        <Bottom>
+          <BottomSheet />
+        </Bottom>
       </Body>
     </Container>
   );
@@ -59,28 +62,6 @@ const Body = styled.div`
   width: 100%;
   height: 100vh;
   background-color: aliceblue;
-`;
-
-const CreatePodButton = styled.button`
-  background-color: #1edd81;
-  color: #fff;
-  font-size: 16px;
-  padding: 12px;
-  text-align: center;
-  border: none;
-  cursor: pointer;
-  z-index: 1000001;
-  font-family: pretendard;
-  font-style: normal;
-  font-weight: 800;
-  width: 114px;
-  height: 48px;
-  border-radius: 24px;
-  position: absolute;
-  bottom: 15%;
-  right: 7%;
-
-  box-shadow: 0px 4px 4px rgba(171, 171, 171, 0.25);
 `;
 
 export default DestinationPage;
