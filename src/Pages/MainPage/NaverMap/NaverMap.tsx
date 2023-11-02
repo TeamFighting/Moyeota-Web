7de20fb41afef793146ab891b4d8444f175afd99
hyorish03 <hyorish2001@gmail.com>
2023-11-02 16:36:17 +0900

    Feat: 키워드에 대한 주소 값 배열에 넣기

--- src/Pages/MainPage/NaverMap/NaverMap.tsx
+++ src/Pages/MainPage/NaverMap/NaverMap.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
+import ContentStore from "../../../zustand/store/ContentStore";
+import axios from "axios";
 
 declare global {
   interface Window {
@@ -10,14 +12,48 @@ declare global {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
 function NaverMap() {
   const mapElement = useRef(null);
+
   const { naver } = window;
+
   const latitude = Number(localStorage.getItem("latitude"));
+
   const longitude = Number(localStorage.getItem("longitude"));
+
+  const { totalData } = ContentStore();
+
+  const destination = useMemo(
+    () => totalData.map((data) => data.destination),
+    [totalData]
+  );
+
+  const [array, setArray] = useState([{}]);
+
+  useEffect(() => {
+    const fetchDestinations = async () => {
+      const promises = destination.map((data) =>
+        axios.get(`http://moyeota.shop/api/distance/keyword`, {
+          params: { query: `${data}` },
+        })
+      );
+      const results = await Promise.all(promises);
+      const data = results.map((result) => result.data);
+      console.log("data", data);
+      setArray(data);
+    };
+
+    fetchDestinations();
+  }, [destination]);
+
+  console.log("array", array);
 
   useEffect(() => {
     if (!mapElement.current || !naver) return;
 
     const location = new naver.maps.LatLng(latitude, longitude);
+
+    // const markerlocation = array.map(
+    //   (data) => new naver.maps.LatLng(data.data.x, longitude)
+    // );
     const mapOptions = {
       center: location,
       zoom: 15,
@@ -25,6 +61,7 @@ function NaverMap() {
     };
 
     const map = new naver.maps.Map(mapElement.current, mapOptions);
+
     new naver.maps.Marker({
       position: location,
       map,
