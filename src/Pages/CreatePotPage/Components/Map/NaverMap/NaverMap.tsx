57c9f51a92f266f301577f7a1646a1c9816212dc
wldnjs7064 <wldnjs7064@naver.com>
2023-11-04 01:49:55 +0900

    feat: 도로명 주소로 검색 시 지도 변경 기능

--- src/Pages/CreatePotPage/Components/Map/NaverMap/NaverMap.tsx
+++ src/Pages/CreatePotPage/Components/Map/NaverMap/NaverMap.tsx
@@ -19,27 +19,53 @@ function NaverMap({ destination }: NaverMapProps) {
     if (!mapElement.current || !naver) return;
 
     if (destination) {
-      const [latitude, longitude] = destination.split(",").map(parseFloat);
-      const location = new naver.maps.LatLng(latitude, longitude);
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const { latitude, longitude } = position.coords;
+          const location = new naver.maps.LatLng(latitude, longitude);
 
-      const mapOptions = {
-        center: location,
-        zoom: 15,
-        zoomControl: false,
-      };
+          const mapOptions = {
+            center: location,
+            zoom: 15,
+            zoomControl: false,
+          };
 
-      const map = new naver.maps.Map(mapElement.current, mapOptions);
+          const map = new naver.maps.Map(mapElement.current, mapOptions);
 
-      new naver.maps.Marker({
-        position: location,
-        map,
-        icon: {
-          url: "../../../public/svg/DestinationLocationIcon.svg",
-          size: new naver.maps.Size(50, 52),
-          origin: new naver.maps.Point(0, 0),
-          anchor: new naver.maps.Point(25, 26),
-        },
-      });
+          naver.maps.Service.geocode(
+            { address: destination },
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            function (status: any, response: any) {
+              if (status === naver.maps.Service.Status.ERROR) {
+                return alert("주소 변환 오류");
+              }
+              if (response.v2 && response.v2.addresses.length > 0) {
+                const new_position = new naver.maps.LatLng(
+                  response.v2.addresses[0].y,
+                  response.v2.addresses[0].x
+                );
+
+                map.setCenter(new_position);
+
+                new naver.maps.Marker({
+                  position: new_position,
+                  map: map,
+                  icon: {
+                    url: "../../../public/svg/DestinationLocationIcon.svg",
+                    size: new naver.maps.Size(50, 52),
+                    origin: new naver.maps.Point(0, 0),
+                    anchor: new naver.maps.Point(25, 26),
+                  },
+                });
+              } else {
+                alert("주소 변환 실패");
+              }
+            }
+          );
+        });
+      } else {
+        console.log("실패");
+      }
     } else {
       if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -56,7 +82,7 @@ function NaverMap({ destination }: NaverMapProps) {
 
           new naver.maps.Marker({
             position: location,
-            map,
+            map: map,
             icon: {
               url: "../../../public/svg/CurrentLocationIcon.svg",
               size: new naver.maps.Size(50, 52),
