f49f60f9340e3f9bbb229e2400104521bc4cc442
wldnjs7064 <wldnjs7064@naver.com>
2023-11-04 02:18:28 +0900

    feat: 키워드를 도로명주소로 변환 api 호출

--- src/Pages/CreatePotPage/Components/Map/NaverMap/NaverMap.tsx
+++ src/Pages/CreatePotPage/Components/Map/NaverMap/NaverMap.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import axios from "axios";
 
 declare global {
   interface Window {
@@ -19,57 +20,14 @@ function NaverMap({ destination }: NaverMapProps) {
     if (!mapElement.current || !naver) return;
 
     if (destination) {
-      if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition((position) => {
-          const { latitude, longitude } = position.coords;
-          const location = new naver.maps.LatLng(latitude, longitude);
-
-          const mapOptions = {
-            center: location,
-            zoom: 15,
-            zoomControl: false,
-          };
-
-          const map = new naver.maps.Map(mapElement.current, mapOptions);
-
-          naver.maps.Service.geocode(
-            { address: destination },
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            function (status: any, response: any) {
-              if (status === naver.maps.Service.Status.ERROR) {
-                return alert("주소 변환 오류");
-              }
-              if (response.v2 && response.v2.addresses.length > 0) {
-                const new_position = new naver.maps.LatLng(
-                  response.v2.addresses[0].y,
-                  response.v2.addresses[0].x
-                );
-
-                map.setCenter(new_position);
-
-                new naver.maps.Marker({
-                  position: new_position,
-                  map: map,
-                  icon: {
-                    url: "../../../public/svg/DestinationLocationIcon.svg",
-                    size: new naver.maps.Size(50, 52),
-                    origin: new naver.maps.Point(0, 0),
-                    anchor: new naver.maps.Point(25, 26),
-                  },
-                });
-              } else {
-                alert("주소 변환 실패");
-              }
-            }
-          );
-        });
-      } else {
-        console.log("실패");
-      }
-    } else {
-      if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition((position) => {
-          const { latitude, longitude } = position.coords;
+      axios
+        .get(`http://moyeota.shop/api/distance/keyword`, {
+          params: { query: destination },
+        })
+        .then((response) => {
+          const data = response.data;
+          const latitude = data.data.y;
+          const longitude = data.data.x;
           const location = new naver.maps.LatLng(latitude, longitude);
 
           const mapOptions = {
@@ -84,16 +42,42 @@ function NaverMap({ destination }: NaverMapProps) {
             position: location,
             map: map,
             icon: {
-              url: "../../../public/svg/CurrentLocationIcon.svg",
+              url: "../../../public/svg/DestinationLocationIcon.svg",
               size: new naver.maps.Size(50, 52),
               origin: new naver.maps.Point(0, 0),
               anchor: new naver.maps.Point(25, 26),
             },
           });
+        })
+        .catch((error) => {
+          console.error("API 호출 오류:", error);
         });
-      } else {
-        console.log("실패");
-      }
+    } else if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        const location = new naver.maps.LatLng(latitude, longitude);
+
+        const mapOptions = {
+          center: location,
+          zoom: 15,
+          zoomControl: false,
+        };
+
+        const map = new naver.maps.Map(mapElement.current, mapOptions);
+
+        new naver.maps.Marker({
+          position: location,
+          map: map,
+          icon: {
+            url: "../../../public/svg/CurrentLocationIcon.svg",
+            size: new naver.maps.Size(50, 52),
+            origin: new naver.maps.Point(0, 0),
+            anchor: new naver.maps.Point(25, 26),
+          },
+        });
+      });
+    } else {
+      console.log("실패");
     }
   }, [destination]);
 
