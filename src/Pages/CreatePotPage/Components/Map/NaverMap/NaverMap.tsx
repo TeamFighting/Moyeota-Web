f232ad6e3245bfb69f76e6e8371beb25c9142c52
wldnjs7064 <wldnjs7064@naver.com>
2023-11-04 01:07:19 +0900

    feat: 네이버 지도 현위치 받아오기

--- src/Pages/CreatePotPage/Components/Map/NaverMap/NaverMap.tsx
+++ src/Pages/CreatePotPage/Components/Map/NaverMap/NaverMap.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
-import ContentStore from "../../../../../zustand/store/ContentStore";
-import axios from "axios";
+import { useEffect, useRef } from "react";
 
 declare global {
   interface Window {
@@ -9,92 +7,40 @@ declare global {
   }
 }
 
-interface ArrayElement {
-  data: {
-    x: number;
-    y: number;
-    place_name: string;
-    road_address_name: string;
-  };
-  status: string;
-  message: string;
-}
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
 function NaverMap() {
   const mapElement = useRef(null);
-
   const { naver } = window;
-
-  const latitude = Number(localStorage.getItem("latitude"));
-
-  const longitude = Number(localStorage.getItem("longitude"));
-
-  const { totalData } = ContentStore();
-
-  const departure = useMemo(
-    () => totalData.map((data) => data.departure),
-    [totalData]
-  );
-
-  const [array, setArray] = useState<ArrayElement[]>([]);
-
-  useEffect(() => {
-    const fetchDestinations = async () => {
-      const promises = departure.map((data) =>
-        axios.get(`http://moyeota.shop/api/distance/keyword`, {
-          params: { query: `${data}` },
-        })
-      );
-      const results = await Promise.all(promises);
-      const data = results.map((result) => result.data);
-      setArray(data);
-    };
-
-    fetchDestinations();
-  }, [departure]);
 
   useEffect(() => {
     if (!mapElement.current || !naver) return;
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        const location = new naver.maps.LatLng(latitude, longitude);
 
-    const location = new naver.maps.LatLng(latitude, longitude);
+        const mapOptions = {
+          center: location,
+          zoom: 15,
+          zoomControl: false,
+        };
 
-    const mapOptions = {
-      center: location,
-      zoom: 15,
-      zoomControl: false,
-    };
-
-    const map = new naver.maps.Map(mapElement.current, mapOptions);
-
-    if (array.length !== 0) {
-      console.log("테스트", array);
-      array.map((data) => {
-        console.log(data);
-        console.log(data.data.x, data.data.y);
+        const map = new naver.maps.Map(mapElement.current, mapOptions);
         new naver.maps.Marker({
-          position: new naver.maps.LatLng(data.data.y, data.data.x),
+          position: location,
           map,
           icon: {
-            url: "../../../public/svg/GreenLocationMarker.svg",
+            url: "../../../public/svg/CurrentLocationIcon.svg",
             size: new naver.maps.Size(50, 52),
             origin: new naver.maps.Point(0, 0),
             anchor: new naver.maps.Point(25, 26),
           },
         });
       });
+    } else {
+      console.log("실패");
     }
-    new naver.maps.Marker({
-      position: location,
-      map,
-      icon: {
-        url: "../../../public/svg/CurrentLocationIcon.svg",
-        size: new naver.maps.Size(50, 52),
-        origin: new naver.maps.Point(0, 0),
-        anchor: new naver.maps.Point(25, 26),
-      },
-    });
-  }, [array]);
+  }, []);
 
   return (
     <>
