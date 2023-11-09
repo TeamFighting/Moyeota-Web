9ab29c90acfff7d6a4766ef522665996d70b5db6
wldnjs7064 <wldnjs7064@naver.com>
2023-11-09 22:49:18 +0900

    fix: PostDataStore 배열에서 객체로 변경

--- src/zustand/store/PostDataStore.ts
+++ src/zustand/store/PostDataStore.ts
@@ -1,14 +1,75 @@
 import create from "zustand";
 
 interface PostDataStoreState {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  data: any[];
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  setPostData: (data: any[]) => void;
+  data: {
+    category: string;
+    content: string;
+    createAt: string;
+    departure: string;
+    departureTime: string;
+    destination: string;
+    distance: number;
+    duration: number;
+    fare: number;
+    numberOfParticipants: number;
+    numberOfRecruitment: number;
+    postId: number;
+    profileImage: string;
+    sameGenderStatus: string;
+    status: string;
+    title: string;
+    userGender: boolean;
+    userName: string;
+    vehicle: string;
+    view: number;
+  };
+  setPostData: (data: {
+    category: string;
+    content: string;
+    createAt: string;
+    departure: string;
+    departureTime: string;
+    destination: string;
+    distance: number;
+    duration: number;
+    fare: number;
+    numberOfParticipants: number;
+    numberOfRecruitment: number;
+    postId: number;
+    profileImage: string;
+    sameGenderStatus: string;
+    status: string;
+    title: string;
+    userGender: boolean;
+    userName: string;
+    vehicle: string;
+    view: number;
+  }) => void;
 }
 
 const usePostDataStore = create<PostDataStoreState>((set) => ({
-  data: [],
+  data: {
+    category: "",
+    content: "",
+    createAt: "",
+    departure: "",
+    departureTime: "",
+    destination: "",
+    distance: 0,
+    duration: 0,
+    fare: 0,
+    numberOfParticipants: 0,
+    numberOfRecruitment: 0,
+    postId: 0,
+    profileImage: "",
+    sameGenderStatus: "",
+    status: "",
+    title: "",
+    userGender: false,
+    userName: "",
+    vehicle: "",
+    view: 0,
+  },
   setPostData: (data) => set({ data }),
 }));
 
