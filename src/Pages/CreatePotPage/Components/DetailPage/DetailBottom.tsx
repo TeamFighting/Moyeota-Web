8e0bc7806a39fc27354fabad23d89effcfa00f2b
wldnjs7064 <wldnjs7064@naver.com>
2023-11-09 23:03:54 +0900

    feat: DetailBottom 수정

--- src/Pages/CreatePotPage/Components/DetailPage/DetailBottom.tsx
+++ src/Pages/CreatePotPage/Components/DetailPage/DetailBottom.tsx
@@ -1,27 +1,23 @@
 import { Calendar, Clock, Dollar } from "../../../../assets/svg";
 import * as S from "./style";
+import usePostDataStore from "../../../../zustand/store/PostDataStore";
 
-interface DetailBottomProps {
-  fare: number;
-  duration: number;
-  splitedTime: string[];
-  timePart: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  data: any;
-  participants: number;
-  recruitment: number;
-}
 function DetailBottom() {
+  const { data } = usePostDataStore();
   return (
     <S.Bottom>
       <S.DescriptionTag>
         <S.Staus>
           <S.Tags>
-            <S.Tag>일반택시</S.Tag>
-            <S.Tag>성별무관</S.Tag>
-            <S.Tag>출퇴근</S.Tag>
+            <S.Tag>{data.vehicle === "일반" ? "일반택시" : "밴택시"}</S.Tag>
+            <S.Tag>
+              {data.sameGenderStatus === "YES" ? "동성끼리" : "성별무관"}
+            </S.Tag>
+            <S.Tag>{data.category}</S.Tag>
           </S.Tags>
-          <S.GateringTag>모집중 1/4</S.GateringTag>
+          <S.GateringTag>
+            모집중 {data.numberOfParticipants}/{data.numberOfRecruitment}
+          </S.GateringTag>
         </S.Staus>
       </S.DescriptionTag>
       <S.TextDescription>
@@ -38,18 +34,20 @@ function DetailBottom() {
           <div style={{ flexDirection: "row", display: "flex", gap: "11px" }}>
             <Calendar width="16" height="16" />
             <div>
-              <div>08월 15일 (화) 오전 11:30 출발</div>
+              <div>{data.departureTime} 출발</div>
             </div>
           </div>
           <div style={{ flexDirection: "row", display: "flex", gap: "11px" }}>
             <Clock width="16" height="16" />
-            <div>30분 소요</div>
+            <div>{Math.floor(data.duration / 60)}분 소요</div>
           </div>
           <div style={{ flexDirection: "row", display: "flex", gap: "11px" }}>
             <Dollar width="16" height="16" />
             <div style={{ flexDirection: "row", display: "flex", gap: "5px" }}>
-              <div style={{ color: "#7E7E7E" }}>예상 금액</div>총 12,700원 -
-              1인당 6,350원 (2인)
+              <div style={{ color: "#7E7E7E" }}>예상 금액</div>총{" "}
+              {data.fare.toLocaleString()}원 - 1인당{" "}
+              {(data.fare / data.numberOfRecruitment).toLocaleString()}원 (
+              {data.numberOfRecruitment}인)
             </div>
           </div>
         </div>
