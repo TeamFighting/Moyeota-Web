cbb61b6c97577063d6ffe2a3708a2abe027bf768
hyorish03 <hyorish2001@gmail.com>
2024-01-02 03:11:32 +0900

    Fix: 이미지 경로 재설정

--- src/Pages/MainPage/NaverMap/NaverMap.tsx
+++ src/Pages/MainPage/NaverMap/NaverMap.tsx
@@ -121,7 +121,7 @@ function NaverMap() {
                 position: position,
                 map: map,
                 icon: {
-                    url: '/public/svg/GreenLocationMarker.svg',
+                    url: '/svg/GreenLocationMarker.svg',
                     size: new naver.maps.Size(50, 52),
                     origin: new naver.maps.Point(0, 0),
                     anchor: new naver.maps.Point(25, 26),
