93dff296ceb6d7ba62672d366bf06a826ef03f34
hyorish03 <hyorish2001@gmail.com>
2024-01-21 19:28:25 +0900

    Fix: 빌드 에러 제거 및 데이터 없을 시 처리

--- src/Pages/MainPage/Components/BottomSheet/BottomSheetContent.tsx
+++ src/Pages/MainPage/Components/BottomSheet/BottomSheetContent.tsx
@@ -1,45 +1,43 @@
 import * as S from '../../style';
 import SingleContent from '../SingleContent';
 import useStore from '../../../../state/store/ContentStore';
-import useBottomSheet from '../../../../Hooks/useBottonSheet';
 const BottomSheetContent = () => {
     const { totalData } = useStore((state) => state);
-    const { content } = useBottomSheet();
-    // if (totalData === undefined || null) {
-    //     return (
-    //         <div
-    //             style={{
-    //                 width: '100%',
-    //                 justifyContent: 'center',
-    //                 alignItems: 'center',
-    //                 display: 'flex',
-    //                 flexDirection: 'column',
-    //                 gap: '50px',
-    //                 marginTop: '100px',
-    //             }}
-    //         >
-    //             <img src="/public/png/Simbol.png" />
-    //             <p
-    //                 style={{
-    //                     fontSize: '32px',
-    //                     fontFamily: 'pretendard',
-    //                     color: ' #9A9A9A',
-    //                     fontWeight: 500,
-    //                 }}
-    //             >
-    //                 데이터가 없습니다 :(
-    //             </p>
-    //         </div>
-    //     );
-    // } else {
-    return (
-        <S.ModalContent>
-            <S.ContentWrapper>
-                <SingleContent />
-            </S.ContentWrapper>
-        </S.ModalContent>
-    );
-    // }
+    if (totalData === undefined || null) {
+        return (
+            <div
+                style={{
+                    width: '100%',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '50px',
+                    marginTop: '100px',
+                }}
+            >
+                <img src="/public/png/Simbol.png" />
+                <p
+                    style={{
+                        fontSize: '32px',
+                        fontFamily: 'pretendard',
+                        color: ' #9A9A9A',
+                        fontWeight: 500,
+                    }}
+                >
+                    데이터가 없습니다 :(
+                </p>
+            </div>
+        );
+    } else {
+        return (
+            <S.ModalContent>
+                <S.ContentWrapper>
+                    <SingleContent />
+                </S.ContentWrapper>
+            </S.ModalContent>
+        );
+    }
 };
 
 export default BottomSheetContent;
