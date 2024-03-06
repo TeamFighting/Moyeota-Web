96e31e6b0a04721d088c9e23a434347dd9acf72f
hyorish03 <hyorish2001@gmail.com>
2024-03-06 20:13:47 +0900

    Feat: 채팅방 내 프로필 이미지 border 설정

--- src/Pages/FirebaseChat/ChatLists.tsx
+++ src/Pages/FirebaseChat/ChatLists.tsx
@@ -1,13 +1,29 @@
 import { useState, useEffect } from 'react';
 import { instance } from '../../axios';
 import { useNavigate } from 'react-router';
+import styled from 'styled-components';
+import { ref as dbRef, child, onChildAdded, limitToLast, query, off, orderByChild, onValue } from 'firebase/database';
+import { db } from '../../firebase';
+import { object } from 'prop-types';
+interface myMessageProps {
+    text: string;
+    timestamp: number;
+    user: {
+        id: string;
+        name: string;
+        profileImage: string;
+    };
+}
 interface ChatRoomProps {
     roomId: string;
+    roomName: string;
     userCount: number;
     createdDate: string;
+    postId: number;
 }
 function ChatLists() {
     const [chatRooms, setChatRooms] = useState<ChatRoomProps[]>([]);
+    const [roomIds, setRoomIds] = useState<string[]>([]);
     const navigate = useNavigate();
     useEffect(() => {
         totalChatRooms();
@@ -20,25 +36,84 @@ function ChatLists() {
         });
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         setChatRooms(res.data.data);
-        console.log(res.data.data);
     };
-    const handleChatRoom = (roomId: string) => {
-        console.log(roomId);
-        navigate(`/chat/${roomId}`, { state: { roomId: roomId } });
+
+    useEffect(() => {
+        chatRooms.map((room) => {
+            setRoomIds((prev) => [...prev, room.roomId]);
+        });
+    }, [chatRooms]);
+
+    const handleChatRoom = (roomId: string, postId: number) => {
+        navigate(`/chat/${postId}`, { state: { roomId: roomId, postId: postId } });
     };
+    const messagesRef = dbRef(db, 'messages');
+    const [lastMessage, setlastMessage] = useState<myMessageProps[]>([]);
+    // useEffect(() => {
+    //     roomIds.length > 0 &&
+    //         roomIds.map((room) => {
+    //             addMessagesListener(room);
+    //         });
+    // }, []);
+
+    // const addMessagesListener = (roomId: string) => {
+    //     onChildAdded(child(messagesRef, roomId), (snapshot) => {
+    //         setlastMessage(snapshot.val());
+    //     });
+    // };
+
+    const lastMessageQuery = query(messagesRef, orderByChild('timestamp'), limitToLast(1));
+    onValue(lastMessageQuery, (snapshot) => {
+        let messages = [];
+        if (snapshot.exists()) {
+            const data = snapshot.val();
+            const lastMessageKey = Object.keys(data)[0];
+            // console.log(data[lastMessageKey]);
+            messages = Object.values(data[lastMessageKey]);
+            console.log(messages);
+
+            messages = Object.keys(data[lastMessageKey]).map((key) => data[key]);
+            // console.log(totalMessages[totalMessages.length - 1]); // 여기서 마지막 메시지의 텍스트가 출력됩니다.
+        } else {
+            console.log('No messages found');
+        }
+    });
+
     const renderChatRooms = () => {
         return (
             chatRooms.length > 0 &&
             chatRooms.map((chatRoom, i) => {
                 return (
-                    <ul key={i} onClick={() => handleChatRoom(chatRoom.roomId)}>
-                        {chatRoom.roomId}
-                    </ul>
+                    <ChatList key={i} onClick={() => handleChatRoom(chatRoom.roomId, chatRoom.postId)}>
+                        {chatRoom.roomName}
+                    </ChatList>
                 );
             })
         );
     };
-    return <div>{renderChatRooms()}</div>;
+    return (
+        <div>
+            <ChatHeader>채팅</ChatHeader>
+            {renderChatRooms()}
+        </div>
+    );
 }
-
+const ChatHeader = styled.div`
+    height: 40px;
+    background-color: aliceblue;
+    display: flex;
+    align-items: center;
+    padding-left: 16px;
+    font-size: 20px;
+    font-weight: bold;
+`;
+const ChatList = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    padding-top: 16px;
+    padding-bottom: 16px;
+    padding-left: 20px;
+    border-bottom: 1px solid #f1f1f1;
+`;
 export default ChatLists;
