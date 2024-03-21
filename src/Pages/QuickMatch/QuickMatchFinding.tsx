fd419cf4fd8e05f369fc5ebadf79b70fb1707a0c
hyorish03 <hyorish2001@gmail.com>
2024-03-22 01:17:22 +0900

    빌드드 에러 수정

--- src/Pages/QuickMatch/QuickMatchFinding.tsx
+++ src/Pages/QuickMatch/QuickMatchFinding.tsx
@@ -2,7 +2,7 @@ import DetailHeader from '../DetailPage/DetailHeader';
 import { ContentDetail, From, Icon, Route, StartPoint, StartPointLocation, Title, Text } from '../DetailPage/style';
 import * as S from './style';
 import { ChevronRight, CurrentLocationIcon, LocationFrom, LocationMarker, ToTriangle } from '../../assets/svg';
-import { useLocation } from 'react-router';
+import { useLocation, useNavigate } from 'react-router';
 import CurrentLocationStore from '../../state/store/CurrentLocation';
 import NaverMap from '../MainPage/NaverMap/NaverMap';
 import styled, { keyframes } from 'styled-components';
