e6b34c990f861febadb3199ea051223eedbc5006
hyorish03 <hyorish2001@gmail.com>
2024-03-25 16:20:43 +0900

    Fix: stroke 관련 옵션 camelcase로 변경

--- src/assets/svg/SelectedOff.tsx
+++ src/assets/svg/SelectedOff.tsx
@@ -1,21 +1,8 @@
-import type { SVGProps } from "react";
+import type { SVGProps } from 'react';
 const SvgSelectedOff = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <circle cx="12" cy="12" r="12" fill="#CDCDCD" />
-    <path
-      d="M8 13L11.5 16L17 9"
-      stroke="white"
-      stroke-width="3"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-  </svg>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <circle cx="12" cy="12" r="12" fill="#CDCDCD" />
+        <path d="M8 13L11.5 16L17 9" stroke="white" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
 );
 export default SvgSelectedOff;
