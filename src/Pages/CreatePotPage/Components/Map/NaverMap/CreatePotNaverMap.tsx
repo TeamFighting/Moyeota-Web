9c78b7322c2f78211b3aff46a797e03a38400376
hyorish03 <hyorish2001@gmail.com>
2024-04-30 19:17:46 +0900

    Feat: 목적지 검색 후 지도에 마커 표시 및 center 이동

--- src/Pages/CreatePotPage/Components/Map/NaverMap/CreatePotNaverMap.tsx
+++ src/Pages/CreatePotPage/Components/Map/NaverMap/CreatePotNaverMap.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef } from 'react';
-import { instance } from '../../../../../axios';
+import { useEffect, useRef, useState } from 'react';
+import LatLngAddStore from '../../../../../state/store/LatLngAddstore';
 
 declare global {
     interface Window {
@@ -11,75 +11,80 @@ declare global {
 interface NaverMapProps {
     destination: string | undefined;
 }
-
 function CreatePotNaverMap({ destination }: NaverMapProps) {
     const mapElement = useRef(null);
     const { naver } = window;
+    const { currentLat, currentLng } = LatLngAddStore((state) => state);
+    const [changeCenter, setChangeCenter] = useState({ lat: currentLat, lng: currentLng });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const [bounds, setBounds] = useState<any>(null);
+    const mapOptions = {
+        zoomControl: false,
+        minZoom: 0,
+        maxZoom: 100,
+        center: new naver.maps.LatLng(changeCenter.lat, changeCenter.lng),
+    };
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let map: any = null;
+
+    function placeSearch(destination: string) {
+        const ps = new kakao.maps.services.Places();
+        if (!destination.replace(/^\s+|\s+$/g, '')) {
+            alert('키워드를 입력해주세요!');
+            return false;
+        }
+        ps.keywordSearch(destination, placesSearchCB);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    function placesSearchCB(data: any, status: any) {
+        if (status === kakao.maps.services.Status.OK) {
+            console.log(data);
+            const boundsArr = [];
+            for (let i = 0; i < data.length; i++) {
+                boundsArr.push(new naver.maps.LatLng(data[i].y, data[i].x));
+            }
+            if (bounds == null) {
+                setChangeCenter({ lat: data[0].y, lng: data[0].x });
+
+                setBounds(new naver.maps.LatLngBounds(boundsArr));
+            }
+            // console.log(bounds);
+
+            displayPlaces(data);
+        }
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const markers: any = [];
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    function displayPlaces(places: any) {
+        for (let key = 0; key < places.length; key++) {
+            const position = new naver.maps.LatLng(places[key].y, places[key].x);
+            // console.log(places[key]);
+            const marker = new naver.maps.Marker({
+                position: position,
+                map: map,
+                icon: {
+                    url: '/svg/GreenLocationMarker.svg',
+                    size: new naver.maps.Size(50, 52),
+                    origin: new naver.maps.Point(0, 0),
+                    anchor: new naver.maps.Point(25, 26),
+                    title: places[key].place_name,
+                },
+            });
+            markers.push(marker);
+        }
+    }
     useEffect(() => {
         if (!mapElement.current || !naver) return;
-
-        if (destination) {
-            instance
-                .get(`/distance/keyword`, {
-                    params: { query: destination },
-                })
-                .then((response) => {
-                    const data = response.data;
-                    const latitude = data.data.y;
-                    const longitude = data.data.x;
-                    const location = new naver.maps.LatLng(latitude, longitude);
-
-                    const mapOptions = {
-                        center: location,
-                        zoom: 15,
-                        zoomControl: false,
-                    };
-
-                    const map = new naver.maps.Map(mapElement.current, mapOptions);
-
-                    new naver.maps.Marker({
-                        position: location,
-                        map: map,
-                        icon: {
-                            url: '/svg/DestinationLocationIcon.svg',
-                            size: new naver.maps.Size(41, 58),
-                            origin: new naver.maps.Point(0, 0),
-                            anchor: new naver.maps.Point(25, 26),
-                        },
-                    });
-                })
-                .catch(() => {
-                    alert('주소를 바르게 입력하세요');
-                });
-        } else if ('geolocation' in navigator) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                const { latitude, longitude } = position.coords;
-                const location = new naver.maps.LatLng(latitude, longitude);
-
-                const mapOptions = {
-                    center: location,
-                    zoom: 15,
-                    zoomControl: false,
-                };
-
-                const map = new naver.maps.Map(mapElement.current, mapOptions);
-
-                new naver.maps.Marker({
-                    position: location,
-                    map: map,
-                    icon: {
-                        url: '/svg/CurrentLocationIcon.svg',
-                        size: new naver.maps.Size(50, 52),
-                        origin: new naver.maps.Point(0, 0),
-                        anchor: new naver.maps.Point(25, 26),
-                    },
-                });
-            });
-        } else {
-            alert('실패');
+        if (mapElement.current) {
+            map = new naver.maps.Map(mapElement.current, mapOptions);
         }
-    }, [destination]);
+        if (destination) {
+            placeSearch(destination);
+        }
+    }, [destination, bounds]);
 
     return (
         <>
