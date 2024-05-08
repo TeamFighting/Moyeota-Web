0cb939e4290900dc92390f7f62b7b0177e7755af
hyorish03 <hyorish2001@gmail.com>
2024-05-08 20:18:04 +0900

    Feat: 팟 만들기 테스트

--- src/Pages/CreatePotPage/Components/Button/CreatePotButton.tsx
+++ src/Pages/CreatePotPage/Components/Button/CreatePotButton.tsx
@@ -6,6 +6,7 @@ import { instance } from '../../../../axios';
 import { useNavigate } from 'react-router-dom';
 import { ref, push, update, child } from 'firebase/database';
 import { db } from '../../../../firebase';
+import { useEffect } from 'react';
 
 function CreatePotButton({ totalPeople }: { totalPeople: number }) {
     // console.log(totalPeople);
@@ -14,7 +15,7 @@ function CreatePotButton({ totalPeople }: { totalPeople: number }) {
     const currentLocationStore = CurrentLocation();
     const navigate = useNavigate();
     const accessToken = localStorage.getItem('accessToken');
-    alert(accessToken);
+
     const chatRoomsRef = ref(db, 'chatRooms');
     const userData = JSON.parse(localStorage.getItem('myInfo') as string);
     const {
@@ -28,75 +29,79 @@ function CreatePotButton({ totalPeople }: { totalPeople: number }) {
     } = potCreateStore;
     alert(selectedTime);
     const { estimatedDuration, estimatedFare } = durationFareStore;
-    const createPost = async () => {
-        const key = push(chatRoomsRef).key;
-        const newChatRoom = {
-            id: key,
-            title: title,
-            createdBy: {
-                user: userData.id,
-                name: userData.name,
-                profileImage: userData.profileImage,
-            },
-        };
-        try {
-            await update(child(chatRoomsRef, key as string), newChatRoom);
-            // console.log('res:', res);
-            const formattedDate = new Date().toISOString;
-            const numberOfRecruitment = totalPeople;
-            const departure = currentLocationStore.currentLocation?.address_name;
-            const response = await instance.post(
-                '/posts',
-                // {
-                //     category: 'LIFE',
-                //     content: '경남쓰팟', //content,
-                //     createdDate: new Date(), //formattedDate,
-                //     departure: '공릉역 7호선', //departure,
-                //     departureTime: new Date(), // selectedTime, //departureTime 으로 바꾸기
-                //     destination: '건대입구역 7호선', //destination,
-                //     distance: 100, // distance,
-                //     duration: '25000', //estimatedDuration,
-                //     fare: '3200', //estimatedFare,
-                //     modifiedDate: new Date(), //formattedDate,
-                //     numberOfRecruitment: 4, // numberOfRecruitment,
-                //     sameGenderStatus: 'YES', //sameGenderStatus,
-                //     title: '경남쓰팟테스트용', //title,
-                //     vehicle: '일반', //vehicle,
-                //     roomId: key,
-                // },
-                {
-                    category: 'LIFE',
-                    content: content,
-                    createdDate: formattedDate,
-                    departure: departure,
-                    departureTime: selectedTime,
-                    destination: destination,
-                    distance: distance,
-                    duration: estimatedDuration,
-                    fare: estimatedFare,
-                    modifiedDate: formattedDate,
-                    numberOfRecruitment: numberOfRecruitment,
-                    sameGenderStatus: sameGenderStatus,
-                    title: title,
-                    vehicle: vehicle,
-                    roomId: key,
-                },
-                {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                        'Content-Type': 'application/json',
-                    },
-                },
-            );
-            console.log(response);
-            if (response.status === 200) {
-                navigate('/createComplete');
-            } else {
-                alert('API 요청 실패');
-            }
-        } catch (error) {
-            console.log(error);
-        }
+    // const createPost = async () => {
+    //     const key = push(chatRoomsRef).key;
+    //     const newChatRoom = {
+    //         id: key,
+    //         title: title,
+    //         createdBy: {
+    //             user: userData.id,
+    //             name: userData.name,
+    //             profileImage: userData.profileImage,
+    //         },
+    //     };
+    //     try {
+    //         await update(child(chatRoomsRef, key as string), newChatRoom);
+    //         // console.log('res:', res);
+    //         const formattedDate = new Date().toISOString;
+    //         const numberOfRecruitment = totalPeople;
+    //         const departure = currentLocationStore.currentLocation?.address_name;
+    //         const response = await instance.post(
+    //             '/posts',
+    //             // {
+    //             //     category: 'LIFE',
+    //             //     content: '경남쓰팟', //content,
+    //             //     createdDate: new Date(), //formattedDate,
+    //             //     departure: '공릉역 7호선', //departure,
+    //             //     departureTime: new Date(), // selectedTime, //departureTime 으로 바꾸기
+    //             //     destination: '건대입구역 7호선', //destination,
+    //             //     distance: 100, // distance,
+    //             //     duration: '25000', //estimatedDuration,
+    //             //     fare: '3200', //estimatedFare,
+    //             //     modifiedDate: new Date(), //formattedDate,
+    //             //     numberOfRecruitment: 4, // numberOfRecruitment,
+    //             //     sameGenderStatus: 'YES', //sameGenderStatus,
+    //             //     title: '경남쓰팟테스트용', //title,
+    //             //     vehicle: '일반', //vehicle,
+    //             //     roomId: key,
+    //             // },
+    //             {
+    //                 category: 'LIFE',
+    //                 content: content,
+    //                 createdDate: formattedDate,
+    //                 departure: departure,
+    //                 departureTime: selectedTime,
+    //                 destination: destination,
+    //                 distance: distance,
+    //                 duration: estimatedDuration,
+    //                 fare: estimatedFare,
+    //                 modifiedDate: formattedDate,
+    //                 numberOfRecruitment: numberOfRecruitment,
+    //                 sameGenderStatus: sameGenderStatus,
+    //                 title: title,
+    //                 vehicle: vehicle,
+    //                 roomId: key,
+    //             },
+    //             {
+    //                 headers: {
+    //                     Authorization: `Bearer ${accessToken}`,
+    //                     'Content-Type': 'application/json',
+    //                 },
+    //             },
+    //         );
+    //         console.log(response);
+    //         if (response.status === 200) {
+    //             navigate('/createComplete');
+    //         } else {
+    //             alert('API 요청 실패');
+    //         }
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // };
+    const createPost = () => {
+        alert(accessToken);
+        alert(selectedTime);
     };
 
     return (
