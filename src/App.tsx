7184a41b91154f4410c74e18f8202c7540642b31
hyorish03 <hyorish2001@gmail.com>
2024-06-05 17:06:12 +0900

    Fix: 구글로그인 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,15 @@ import OAuth2RedirectHandler from './Pages/LoginPage/OAuth';
 function App() {
     const url = new URL(window.location.href);
     const isNaver = url.searchParams.get('state')?.toString();
-    const isGoogle = url.searchParams.get('scope')?.toString();
+    const isGoogle = url.toString().search('google');
     console.log(isGoogle);
     if (isNaver === 'nid/me') {
         OAuth2RedirectHandler({ from: 'NAVER' });
-    } else if (isGoogle === 'email profile openid') {
+    } else if (isGoogle !== -1) {
         OAuth2RedirectHandler({ from: 'GOOGLE' });
     } else {
         OAuth2RedirectHandler({ from: 'KAKAO' });
     }
     return <div>로그인 중..</div>;
 }
-
 export default App;
