efe39f38ad5d9e0d6e62973af4902616b61b110c
hyorish03 <hyorish2001@gmail.com>
2024-07-28 19:44:21 +0900

    Fix: 리사이징된 이미지로 적용

--- src/Pages/MainPage/Components/SingleContent/SingleProfile.tsx
+++ src/Pages/MainPage/Components/SingleContent/SingleProfile.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { instance } from '../../../../axios';
+// import { useEffect, useState } from 'react';
+// import { instance } from '../../../../axios';
 import * as S from '../../style';
 
 interface ProfileProps {
@@ -13,26 +13,26 @@ interface ProfileProps {
 
 function Profile({ profileImg, userName, index, gender, ago, distance }: ProfileProps) {
   console.log('Profile', profileImg);
-  const [resizedImg, setResizedImg] = useState<string>('');
+  // const [resizedImg, setResizedImg] = useState<string>('');
 
-  const getResizedImg = async (img: string) => {
-    // const encodedImgUrl = encodeURIComponent(img);
+  // const getResizedImg = async (img: string) => {
+  //   // const encodedImgUrl = encodeURIComponent(img);
 
-    const res = await instance.get('/users/image-resizing', {
-      params: {
-        imageUrl: img,
-      },
-    });
-    setResizedImg(res.data);
-  };
+  //   const res = await instance.get('/users/image-resizing', {
+  //     params: {
+  //       imageUrl: img,
+  //     },
+  //   });
+  //   setResizedImg(res.data);
+  // };
 
-  useEffect(() => {
-    getResizedImg(profileImg);
-  }, []);
+  // useEffect(() => {
+  //   getResizedImg(profileImg);
+  // }, []);
   return (
     <S.ProfileInfo key={index}>
       <S.ProfileLeft>
-        <img src={resizedImg} width="24px" height="24px" style={{ borderRadius: '100%' }} />
+        <img src={profileImg} width="24px" height="24px" style={{ borderRadius: '100%' }} />
 
         <S.ProfileName>{userName}</S.ProfileName>
         <S.ProfileSex>{gender}</S.ProfileSex>
