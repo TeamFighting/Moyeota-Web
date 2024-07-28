04f5dccfca2af8ec50c8db11cc4e1aee6b06185b
hyorish03 <hyorish2001@gmail.com>
2024-07-28 22:12:27 +0900

    Fix: 이미지 리사이징 제거

--- src/Pages/MainPage/Components/SingleContent/SingleProfile.tsx
+++ src/Pages/MainPage/Components/SingleContent/SingleProfile.tsx
@@ -12,27 +12,10 @@ interface ProfileProps {
 }
 
 function Profile({ profileImg, userName, index, gender, ago, distance }: ProfileProps) {
-  console.log('Profile', profileImg);
-  const [resizedImg, setResizedImg] = useState<string>('');
-
-  const getResizedImg = async (img: string) => {
-    // const encodedImgUrl = encodeURIComponent(img);
-
-    const res = await instance.get('/users/image-resizing', {
-      params: {
-        imageUrl: img,
-      },
-    });
-    setResizedImg(res.data);
-  };
-
-  useEffect(() => {
-    getResizedImg(profileImg);
-  }, []);
   return (
     <S.ProfileInfo key={index}>
       <S.ProfileLeft>
-        <img src={resizedImg} width="24px" height="24px" style={{ borderRadius: '100%' }} />
+        <img src={profileImg} width="24px" height="24px" style={{ borderRadius: '100%' }} />
 
         <S.ProfileName>{userName}</S.ProfileName>
         <S.ProfileSex>{gender}</S.ProfileSex>
