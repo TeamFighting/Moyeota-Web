64d78a69f58c5438b1ba47fd75ed48c616b029e8
hyorish03 <hyorish2001@gmail.com>
2024-07-29 00:04:52 +0900

    Refactor: 사용하지 않는 마이페이지 컴포넌트 제거

--- src/Pages/MyPage/BodyComponents/Lists.tsx
+++ src/Pages/MyPage/BodyComponents/Lists.tsx
@@ -1,9 +1,6 @@
 import { useNavigate } from 'react-router';
 import { ChevronRight } from '../../../assets/svg';
-import SvgBell from '../../../assets/svg/Bell';
 import SvgCreditcard from '../../../assets/svg/Creditcard';
-import SvgSpeakerphone from '../../../assets/svg/Speakerphone';
-import SvgStar from '../../../assets/svg/Star';
 import SvgTaxi from '../../../assets/svg/Taxi';
 import SvgUser from '../../../assets/svg/User';
 import * as S from '../MyPage_styles';
@@ -20,11 +17,11 @@ function Lists({ userId }: Props) {
         { id: 2, title: '계좌 관리', icon: <SvgCreditcard width={24} />, link: `/mypage/editAccount/${userId}` },
         { id: 3, title: '팟 이용내역', icon: <SvgTaxi width={24} />, link: '' },
     ];
-    const lists2 = [
-        { id: 4, title: '즐겨찾는 장소 설정', icon: <SvgStar width={24} />, link: '' },
-        { id: 5, title: '알림 설정', icon: <SvgBell width={24} />, link: '' },
-        { id: 6, title: '공지사항', icon: <SvgSpeakerphone width={24} />, link: '' },
-    ];
+    // const lists2 = [
+    //     { id: 4, title: '즐겨찾는 장소 설정', icon: <SvgStar width={24} />, link: '' },
+    //     { id: 5, title: '알림 설정', icon: <SvgBell width={24} />, link: '' },
+    //     { id: 6, title: '공지사항', icon: <SvgSpeakerphone width={24} />, link: '' },
+    // ];
     const navigateTo = (link: string) => {
         if (link === '') return alert('준비중인 서비스입니다.');
         navigate(link);
@@ -40,22 +37,22 @@ function Lists({ userId }: Props) {
             </S.List>
         );
     });
-    const RenderList2 = lists2.map((list) => {
-        return (
-            <S.List onClick={() => navigateTo(list.link)} key={list.id}>
-                <S.ListLeft>
-                    {list.icon}
-                    <S.ListText>{list.title}</S.ListText>
-                </S.ListLeft>
-                <ChevronRight width={24} />
-            </S.List>
-        );
-    });
+    // const RenderList2 = lists2.map((list) => {
+    //     return (
+    //         <S.List onClick={() => navigateTo(list.link)} key={list.id}>
+    //             <S.ListLeft>
+    //                 {list.icon}
+    //                 <S.ListText>{list.title}</S.ListText>
+    //             </S.ListLeft>
+    //             <ChevronRight width={24} />
+    //         </S.List>
+    //     );
+    // });
     return (
         <S.ListWrapper>
             {RenderList1}
-            <div style={{ width: '100%', height: '6px', backgroundColor: '#F5F6F8' }} />
-            {RenderList2}
+            {/* <div style={{ width: '100%', height: '6px', backgroundColor: '#F5F6F8' }} /> */}
+            {/* {RenderList2} */}
         </S.ListWrapper>
     );
 }
