8c9f17baf834fc01710f446392dc6e6a5d8a7659
hyorish03 <hyorish2001@gmail.com>
2024-08-01 22:06:03 +0900

    Feat: 사용자 접속 기기 확인 훅 구현

--- src/Pages/LoginPage/index.tsx
+++ src/Pages/LoginPage/index.tsx
@@ -3,14 +3,17 @@ import Body from './Body';
 import Header from './Header';
 import { useEffect } from 'react';
 import watchPositionHook from '../../Hooks/useWatchPositionHook';
+import useIsMobile from '../../Hooks/useIsMobile';
 
 function LoginPage() {
     watchPositionHook();
+    const isMobile = useIsMobile();
     useEffect(() => {
         const haveToken = sessionStorage.getItem('accessToken');
         if (haveToken !== null) {
             window.location.href = '/mainpage';
         }
+        alert(isMobile);
     }, []);
 
     return (
