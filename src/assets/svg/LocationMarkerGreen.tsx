dea0b97a5125cf4972c8222dd99eceae67ee92a3
hyorish03 <hyorish2001@gmail.com>
2024-08-19 22:54:12 +0900

    Fix: deprecated 된 svg 속성 변경

--- src/assets/svg/LocationMarkerGreen.tsx
+++ src/assets/svg/LocationMarkerGreen.tsx
@@ -1,19 +1,12 @@
-import type { SVGProps } from "react";
+import type { SVGProps } from 'react';
 const SvgLocationMarkerGreen = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    width="24"
-    height="25"
-    viewBox="0 0 24 25"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M17.9397 4.89829C21.2201 8.1787 21.2201 13.4973 17.9397 16.7777L12 22.7174L6.06028 16.7777C2.77987 13.4973 2.77987 8.1787 6.06028 4.89829C9.34068 1.61789 14.6593 1.61789 17.9397 4.89829ZM12 13.238C13.3255 13.238 14.4 12.1635 14.4 10.838C14.4 9.51251 13.3255 8.43799 12 8.43799C10.6745 8.43799 9.59998 9.51251 9.59998 10.838C9.59998 12.1635 10.6745 13.238 12 13.238Z"
-      fill="#1EDD81"
-    />
-  </svg>
+    <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M17.9397 4.89829C21.2201 8.1787 21.2201 13.4973 17.9397 16.7777L12 22.7174L6.06028 16.7777C2.77987 13.4973 2.77987 8.1787 6.06028 4.89829C9.34068 1.61789 14.6593 1.61789 17.9397 4.89829ZM12 13.238C13.3255 13.238 14.4 12.1635 14.4 10.838C14.4 9.51251 13.3255 8.43799 12 8.43799C10.6745 8.43799 9.59998 9.51251 9.59998 10.838C9.59998 12.1635 10.6745 13.238 12 13.238Z"
+            fill="#1EDD81"
+        />
+    </svg>
 );
 export default SvgLocationMarkerGreen;
