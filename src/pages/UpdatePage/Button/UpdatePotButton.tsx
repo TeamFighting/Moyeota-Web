75974c1eccac6e5cd4b2c3a45c2d30e48ead0ca6
hyorish03 <hyorish2001@gmail.com>
2024-08-19 23:32:28 +0900

    Fix: 팟 생성/수정 type 변수 명 통일

--- src/pages/UpdatePage/Button/UpdatePotButton.tsx
+++ src/pages/UpdatePage/Button/UpdatePotButton.tsx
@@ -2,101 +2,62 @@ import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import PotCreateStore from '../../../stores/PotCreateStore';
 import DurationFareStore from '../../../stores/DurationFareStore';
-import usePostDataStore from '../../../stores/PostDataStore';
+import instance from '@apis/index';
+import DestinationStore from '@stores/DestinationResult';
 
-function CreatePotButton({ totalPeople, roomId, postId }: { totalPeople: number; roomId: string; postId: number }) {
+function CreatePotButton({ roomId, postId }: { roomId: string; postId: number }) {
     const navigate = useNavigate();
-    const { data } = usePostDataStore();
+    const { title, sameGenderStatus, destination, VehicleType } = PotCreateStore();
+    const { finalDestination } = DestinationStore();
     const currentDate = new Date();
     const formattedDate = currentDate.toISOString();
-    const { distance, selectedTime } = PotCreateStore();
+    const { distance, selectedTime, content, totalPeople } = PotCreateStore();
     const { estimatedDuration, estimatedFare } = DurationFareStore();
-    const { content, destination, sameGenderStatus, vehicle, title } = data;
     const numberOfRecruitment = totalPeople;
     const departure = sessionStorage.getItem('address');
-    const accessToken = sessionStorage.getItem('accessToken');
+    const accessToken = localStorage.getItem('accessToken');
     const latitude = sessionStorage.getItem('latitude');
     const longitude = sessionStorage.getItem('longitude');
-    // console.log('content', content);
-    // console.log('departure', departure);
-    // console.log('createdDate', formattedDate);
-    // console.log(
-    //     'departureTime',
-    //     selectedTime, //departureTime으로 바꾸기
-    // );
-    // console.log('destination', destination);
-    // console.log('distance', distance);
-    // console.log('duration', estimatedDuration);
-    // console.log('fare', estimatedFare);
-    // console.log('numberOfRecruitment', numberOfRecruitment);
-    // console.log('sameGenderStatus', sameGenderStatus);
-    // console.log('title', title);
-    // console.log('vehicle', vehicle);
-    // console.log('latitude', latitude);
-    // console.log('longitude', longitude);
-    // console.log('modifiedDate', formattedDate);
-    // console.log('roomId', roomId);
+
     const updatePost = async () => {
         try {
-            const response = await fetch(`/posts/${postId}`, {
-                method: 'PATCH',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${accessToken}`,
-                },
-                body: JSON.stringify({
+            const response = await instance.patch(
+                `/posts/${postId}`,
+                {
                     category: 'LIFE',
-                    content: content,
+                    content: content || '',
                     departure: departure,
                     createdDate: formattedDate,
                     departureTime: selectedTime, //departureTime으로 바꾸기
-                    destination: destination,
+                    destination: finalDestination || destination,
                     distance: distance,
                     duration: estimatedDuration,
                     fare: estimatedFare,
                     numberOfRecruitment: numberOfRecruitment,
                     sameGenderStatus: sameGenderStatus,
                     title: title,
-                    vehicle: vehicle,
+                    vehicle: VehicleType,
                     latitude: latitude,
                     longitude: longitude,
                     modifiedDate: formattedDate,
                     roomId: roomId,
-                }),
-            });
-
-            /**
-             *{
-  "category": "LIFE",
-  "content": "같이 갈 사람 참가 신청 ㄱㄱ",
-  "createdDate": "2024-05-16T12:02:27.071Z",
-  "departure": "공릉역 7호선",
-  "departureTime": "2024-05-16T12:02:27.071Z",
-  "destination": "서울과학기술대학교 어의관",
-  "distance": 0.5,
-  "duration": 313,
-  "fare": 5200,
-  "latitude": "string",
-  "longitude": "string",
-  "modifiedDate": "2024-05-16T12:02:27.071Z",
-  "numberOfRecruitment": 4,
-  "roomId": "uuid",
-  "sameGenderStatus": "YES",
-  "title": "공릉역에서 어의관 갈사람?",
-  "vehicle": "일반"
-}
-             */
-
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                },
+            );
+            console.log('response:', response);
             if (response.status == 200) {
                 alert('팟이 성공적으로 수정되었습니다.');
-                navigate(`/CreateDetailPage`);
+                navigate(`/detailPage/${postId}`);
             } else {
-                const errorData = await response.json();
-                console.error('팟 수정에 실패했습니다.', response.status, errorData);
+                console.error('팟 수정에 실패했습니다.', response.status);
                 alert(response);
             }
         } catch (e: any) {
-           
             console.error('팟 수정 중 오류 발생', e);
         }
     };
