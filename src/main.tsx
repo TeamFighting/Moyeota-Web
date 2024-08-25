6639e24ed52368e9ab743fb19c8d981198f92632
hyorish03 <hyorish2001@gmail.com>
2024-08-26 02:03:44 +0900

    Refactor: 라우팅 컴포넌트 분리로 인한 main.tsx 정리

--- src/main.tsx
+++ src/main.tsx
@@ -1,170 +1,22 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import App from './App';
-import MainPage from './pages/MainPage';
-import DetailPage from './pages/DetailPage';
-import CreatePotPage from './pages/CreatePotPage';
-import CreateComplete from './pages/CreatePotPage/CreateComplete';
-import DestinationPage from './pages/CreatePotPage/Components/Map/CreateDestinationMapPage';
-import QuickMatch from './pages/QuickMatch/QuickMatch';
-import QuickMatchFinding from './pages/QuickMatch/QuickMatchFinding';
-import FirebaseChat from './pages/FirebaseChat/views';
-import ChatLists from './pages/FirebaseChat/views/ChatLists';
-import UpdatePotPage from './pages/UpdatePage';
-import PotPage from './pages/MyPage/MyPot';
-import OwnerReimbursement from './pages/ReimbursementPage/OwnerReimbursement';
-import ApplierReimbusement from './pages/ReimbursementPage/ApplierReimbursement';
-import AddAccount from './pages/AddBankAccount';
-import OwnerCalc from './pages/ReimbursementPage/Calculation/OwnerCalc';
-import ApplierCalc from './pages/ReimbursementPage/Calculation/ApplierCalc';
-import BankRecommend from './pages/AddBankAccount/BankListSheet/BankRecommend';
-import WaitPlease from './pages/ReimbursementPage/Calculation/WaitPlease';
-import CurrentReimbursement from './pages/ReimbursementPage/CurrentReimbursement';
+import { BrowserRouter } from 'react-router-dom';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import LoginPage from './pages/LoginPage';
-import MyPage from './pages/MyPage';
-import EditAccount from './pages/MyPage/MyPageLists/EditBankAccount';
-import ModifyProfile from './pages/MyPage/MyPageLists/ModifyNickName';
-import ManageProfile from './pages/MyPage/MyPageLists/ManageAccount';
-import { ROUTE } from '@constants/route';
 import ToastProvider from 'ToastProvider';
-import SelectGenderAge from '@pages/SelectGenderAge';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
-const routes = [
-    {
-        path: ROUTE.ROOT,
-        element: <App />,
-    },
+import App from 'App';
 
-    {
-        path: ROUTE.MAIN_PAGE,
-        element: <MainPage />,
-    },
-    {
-        path: ROUTE.DETAIL_PAGE,
-        element: <DetailPage />,
-    },
-    {
-        path: ROUTE.CREATE_POT_PAGE,
-        element: <CreatePotPage />,
-    },
-    {
-        path: ROUTE.CREATE_COMPLETE,
-        element: <CreateComplete />,
-    },
-    {
-        path: ROUTE.DESTINATION_PAGE,
-        element: <DestinationPage />,
-    },
-
-    {
-        path: ROUTE.QUICK_MATCH,
-        element: <QuickMatch />,
-    },
-    {
-        path: ROUTE.QUICK_MATC_HFINDING,
-        element: <QuickMatchFinding />,
-    },
-
-    {
-        path: ROUTE.UPDATEPOT,
-        element: <UpdatePotPage />,
-    },
-    {
-        path: ROUTE.CHAT,
-        element: <FirebaseChat />,
-    },
-    {
-        path: ROUTE.CHATLISTS,
-        element: <ChatLists />,
-    },
-    {
-        path: ROUTE.POTPAGE,
-        element: <PotPage />,
-    },
-    {
-        path: ROUTE.REIMBURSEMENT_POTOWNER,
-        element: <OwnerReimbursement />,
-    },
-    {
-        path: ROUTE.REIMBURSEMENT,
-        element: <ApplierReimbusement />,
-    },
-    {
-        path: ROUTE.REIMBURSEMENT_CURRENT,
-        element: <CurrentReimbursement />,
-    },
-    {
-        path: ROUTE.ADD_ACCOUNT,
-        element: <AddAccount />,
-    },
-    {
-        path: ROUTE.OWNERCALC,
-        element: <OwnerCalc />,
-    },
-    {
-        path: ROUTE.APPLIERCALC,
-        element: <ApplierCalc />,
-    },
-    {
-        path: ROUTE.BANK_RECOMMEND,
-        element: <BankRecommend />,
-    },
-    {
-        path: ROUTE.WAIT_PLEASE,
-        element: <WaitPlease />,
-    },
-    {
-        path: ROUTE.LOGIN,
-        element: <LoginPage />,
-    },
-    {
-        path: ROUTE.MYPAGE,
-        element: <MyPage />,
-    },
-    {
-        path: ROUTE.MYPAGE_MODIFY,
-        element: <ModifyProfile />,
-    },
-    {
-        path: ROUTE.MYPAGE_EDIT_ACCOUNT,
-        element: <EditAccount />,
-    },
-    {
-        path: ROUTE.MYPAGE_MANAGE_PROFILE,
-        element: <ManageProfile />,
-    },
-    {
-        path: ROUTE.MYPAGE_MANAGE_PROFILE,
-        element: <ManageProfile />,
-    },
-    {
-        path: ROUTE.SELECT_GENDER_AGE,
-        element: <SelectGenderAge />,
-    },
-];
-
-// async function enableMock() {
-//     if (import.meta.env.VITE_DEVELOPMENT !== 'devMode') {
-//         return;
-//     }
-
-//     return worker.start();
-// }
-
-// enableMock().then(() => {
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.tz.setDefault('Asia/Seoul');
-const router = createBrowserRouter(routes);
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <RouterProvider router={router} />
-        <ToastProvider />
+        <BrowserRouter>
+            <App />
+            <ToastProvider />
+        </BrowserRouter>
     </LocalizationProvider>,
 );
-// });
