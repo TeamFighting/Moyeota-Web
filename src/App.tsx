954e865cfaaf9c70b515aa0cad5e8719ebbac856
hyorish03 <hyorish2001@gmail.com>
2024-08-26 02:03:10 +0900

    Refactor: 기존 app.tsx를 loginloading.tsx로 변경 및 app.tsx에 setScreenHeight 함수 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { OAUTH_PROVIDER } from '@pages/LoginPage/consts';
-import handleOAuth2Redirect from './pages/LoginPage/OAuth';
-import { useNavigate } from 'react-router';
+import { useEffect } from 'react';
+import { Router } from 'Router';
 function App() {
-    const navigate = useNavigate();
-    const url = new URL(window.location.href);
-    const isNaver = url.searchParams.get('state')?.toString();
-    const isGoogle = url.toString().search('google');
-    if (isNaver === 'nid/me') {
-        handleOAuth2Redirect({ from: OAUTH_PROVIDER.NAVER, navigate });
-    } else if (isGoogle !== -1) {
-        handleOAuth2Redirect({ from: OAUTH_PROVIDER.GOOGLE, navigate });
-    } else {
-        handleOAuth2Redirect({ from: OAUTH_PROVIDER.KAKAO, navigate });
-    }
-    return <div>로그인 중..</div>;
+    const setScreenHeight = () => {
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+    useEffect(() => {
+        setScreenHeight();
+        window.addEventListener('resize', setScreenHeight);
+        return () => {
+            window.removeEventListener('resize', setScreenHeight);
+        };
+    }, []);
+    return <Router />;
 }
 export default App;
