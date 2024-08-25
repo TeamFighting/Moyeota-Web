102ff10a7ed1e2c087e0cbd1cf18efeb0b644b11
hyorish03 <hyorish2001@gmail.com>
2024-08-25 18:00:08 +0900

    Feat: 모킹된 사용자 조회 API를 실제 서버 API로 변경

--- src/pages/MyPage/MyPageLists/EditBankAccount/index.tsx
+++ src/pages/MyPage/MyPageLists/EditBankAccount/index.tsx
@@ -1,10 +1,11 @@
 import Header from '../Header';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import { BankAccountInfo, BankAccountNumber, BankInfoTop, BankListWrapper, BankLogo, BankName, Wrapper } from './style';
 import { useNavigate, useParams } from 'react-router';
 import BottomNav from '@components/BottomNav';
 import { FloatingBottomButton } from '@components/Buttons/FloatingBottomButton';
+import instance from '@apis/index';
+import { BankLists } from '@assets/BankLists';
 
 type BankAccount = {
     bankName: string;
@@ -15,15 +16,19 @@ function EditBankAccount() {
     const [bankAccount, setBankAccount] = useState<BankAccount[]>([{ bankName: '', accountNumber: '' }]);
     const userId = useParams().userId;
     const navigate = useNavigate();
-
+    const accessToken = localStorage.getItem('accessToken');
     useEffect(() => {
         getBankAccount();
     }, []);
     const getBankAccount = async () => {
         try {
-            const re = await axios.get('/users/account');
-            console.log(re.data);
-            setBankAccount(re.data);
+            const re = await instance.get('/accounts', {
+                headers: {
+                    Authorization: 'Bearer ' + accessToken,
+                },
+            });
+            console.log(re.data.data);
+            setBankAccount(re.data.data);
         } catch (e) {
             console.log(e);
         }
@@ -36,15 +41,20 @@ function EditBankAccount() {
         <Wrapper>
             <Header title={'계좌 관리'} />
             <BankListWrapper style={{ width: '100%', height: '100%', overflowY: 'scroll' }}>
-                {bankAccount.map((account, index) => (
-                    <BankAccountInfo key={index}>
-                        <BankInfoTop>
-                            <BankLogo></BankLogo>
-                            <BankName>{account.bankName}</BankName>
-                        </BankInfoTop>
-                        <BankAccountNumber>{account.accountNumber}</BankAccountNumber>
-                    </BankAccountInfo>
-                ))}
+                {bankAccount &&
+                    bankAccount.map((account, index) => {
+                        const url = BankLists.find((bank) => bank.name === account.bankName)?.url || '';
+
+                        return (
+                            <BankAccountInfo key={index}>
+                                <BankInfoTop>
+                                    <BankLogo src={url}></BankLogo>
+                                    <BankName>{account.bankName}</BankName>
+                                </BankInfoTop>
+                                <BankAccountNumber>{account.accountNumber}</BankAccountNumber>
+                            </BankAccountInfo>
+                        );
+                    })}
             </BankListWrapper>
             <FloatingBottomButton message="계좌번호 추가하기" onClickFunction={navToAddAccount} />
             <BottomNav />
