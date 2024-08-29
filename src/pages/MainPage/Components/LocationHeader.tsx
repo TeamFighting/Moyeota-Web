d65fd9c2ea90a0fcb6992e9d571a8a5c1c785587
hyorish03 <hyorish2001@gmail.com>
2024-08-29 14:18:03 +0900

    Chore: 사용하지 않는 변수 삭제

--- src/pages/MainPage/Components/LocationHeader.tsx
+++ src/pages/MainPage/Components/LocationHeader.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import styled from 'styled-components';
 import CurrentLocationStore from '../../../stores/CurrentLocation';
 import LatLngAddStore from '@stores/LatLngAddstore';
