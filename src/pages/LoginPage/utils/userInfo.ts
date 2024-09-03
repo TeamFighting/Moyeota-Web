da1c8b2d7d3f59151840fafffeb92a7fa40a781b
hyorish03 <hyorish2001@gmail.com>
2024-09-03 19:10:09 +0900

    Chore: userInfo.ts 삭제

--- src/pages/LoginPage/utils/userInfo.ts
+++ src/pages/LoginPage/utils/userInfo.ts
@@ -1,27 +1,27 @@
-import instance from '@apis/index';
-import { useMyInfoStore } from '@stores/MyInfo';
-import { getAccessToken } from '@utils/getAccessToken';
+// import instance from '@apis/index';
+// import { useMyInfoStore } from '@stores/MyInfo';
+// import { getAccessToken } from '@utils/getAccessToken';
 
-export function useUserInfo() {
-    const { setMyInfo } = useMyInfoStore();
+// export function useUserInfo() {
+//     const { setMyInfo } = useMyInfoStore();
 
-    async function fetchUserInfo() {
-        const accessToken = getAccessToken();
-        alert('alertAccessToken: ' + accessToken);
-        try {
-            const res = await instance.get('/users', {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
-            });
-            if (res.status === 200) {
-                setMyInfo(res.data.data);
-                localStorage.setItem('myInfo', JSON.stringify(res.data.data));
-            }
-        } catch (e: any) {
-            alert('사용자 정보를 불러오는데 실패했습니다.');
-        }
-    }
+//     async function fetchUserInfo() {
+//         const accessToken = getAccessToken();
+//         alert('alertAccessToken: ' + accessToken);
+//         try {
+//             const res = await instance.get('/users', {
+//                 headers: {
+//                     Authorization: `Bearer ${accessToken}`,
+//                 },
+//             });
+//             if (res.status === 200) {
+//                 setMyInfo(res.data.data);
+//                 localStorage.setItem('myInfo', JSON.stringify(res.data.data));
+//             }
+//         } catch (e: any) {
+//             alert('사용자 정보를 불러오는데 실패했습니다.');
+//         }
+//     }
 
-    return { fetchUserInfo };
-}
+//     return { fetchUserInfo };
+// }
